83ecb3dd61ceb161f624ce9dd1319ae7249f371d
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 11:13:29 +0300

    numerical data types

--- week2/main.py
+++ week2/main.py
@@ -16,6 +16,15 @@ print("\n Var 1 : ", var1, "is of type",type(var1))
 print("Var 2 : ", var2, "is of type", type(var2))
 print("Var 3 :", var3, "is of type", type(var3))
 print("Var 4 :", var4, "of type", type(var4))
-print("Var 5 :", var5, "of type", type(var5))
+print("Var 5 :", var5, "of type", type(var5), "\n")
+
+# try this
+c = 20.345
+d = 10+5j
+sum = c + d
+print("C is of type: ", type(c), "\n")
+print("D is of type: ", type(d), "\n")
+print("Sum of (c +d)  is: ", sum, "of type", type(sum), "\n")
+
 
 
