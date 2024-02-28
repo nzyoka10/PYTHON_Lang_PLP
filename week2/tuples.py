18deb54eedd31bae47572bcf0976e054cbfac65a
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 12:13:03 +0300

    python tuples

---
+++ week2/tuples.py
@@ -0,0 +1,13 @@
+# PYTHON Tuple data type
+# Tuples are ordered and immutable. 
+# They can contain elements of any data type, including other tuples. 
+
+myTuple = ('ABCD', 56,  'Nairobi', 3+7j, 70.2)
+# print(myTuple)
+# slicing tuple
+# print(myTuple[1:4])   # Output: ( 56, 'Nairobi', 3+7j)
+                       # The slice starts from index  1 and ends at index 4 (not inclusive).
+# print(myTuple * 3)
+tinyTuple = 'Wed', 'Mon', 'Fri', 34.7
+print(myTuple + tinyTuple)
+
