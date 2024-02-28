84fe0472f5a730e44129d99bc8c402f03295d165
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 11:05:30 +0300

    pyhton data types

---
+++ week2/main.py
@@ -0,0 +1,21 @@
+# ptyhon data types
+
+# numerical data type
+var1 = 47 # integer
+var3 = 4.5 # float
+var4 = 10+3j # complex
+
+# String data type
+var5 = "PYTHON"
+
+# Boolean data type
+var2 = True # boolean
+
+
+print("\n Var 1 : ", var1, "is of type",type(var1))
+print("Var 2 : ", var2, "is of type", type(var2))
+print("Var 3 :", var3, "is of type", type(var3))
+print("Var 4 :", var4, "of type", type(var4))
+print("Var 5 :", var5, "of type", type(var5))
+
+
