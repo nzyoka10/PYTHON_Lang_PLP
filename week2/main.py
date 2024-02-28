85dab0bec9539ecd5602862f14685f985a69bfa2
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 11:38:44 +0300

    pythong string operations

--- week2/main.py
+++ week2/main.py
@@ -1,5 +1,8 @@
-# ptyhon data types
+# python data types
+# String operations --> 
+# slicing  and concatenation strings
 
+'''
 # numerical data type
 var1 = 47 # integer
 var3 = 4.5 # float
@@ -25,6 +28,34 @@ sum = c + d
 print("C is of type: ", type(c), "\n")
 print("D is of type: ", type(d), "\n")
 print("Sum of (c +d)  is: ", sum, "of type", type(sum), "\n")
+'''
+
+# sequence data type
+# 1. strings, 2. lists, 3. tuples, 4. range
+
+# string data type
+name = "hello PLP Academy"
+# print(name)
+# print(type(name))
+
+# string data type operations
+# 1. str Slicing 2. str Concatenation
+# print(name[:]) # prints the complete _string
+
+# string slicing
+# print(name[0]) # print first item of the string
+# print(name[6:10]) # print second item of the string
+# print(name[-1]) # print last item
+
+# STRING CONCATENATION
+print(name + "\n Welcome to Python Programming Language.\n")
+print(name * 4)
+
+
+
+# list data type
+# seq_list = [1, 'a', 'b', 'c', 9]
+# print("seq_list is -> ", seq_list, "of type: ", type(seq_list), "\n")
 
 
 
