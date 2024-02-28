bd0af7b23550e573d5650f253183ede1453cdf57
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 12:42:26 +0300

    python data types

--- main.py
+++ main.py
@@ -3,5 +3,16 @@
     This tutorial shows how to read user's keyboard input. The code is written in Python and displaying it on the screen.
 '''
 
-name = input('Enter your name: ')
-print(name)
+# name = input('Enter your name: ')
+# print(name)
+
+# x = input('Enter a number: ')
+# print(type(x))
+
+# print('What is your name?')
+# name = input()
+# print(name)
+
+# Converting numbers to strings
+x = 5
+print('The number is ' + str(x))
