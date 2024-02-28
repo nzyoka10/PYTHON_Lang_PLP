79c180db6f631992b17d062697ccefcc0dc741d1
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 17:14:16 +0300

    more of python data types

---
+++ index.py
@@ -0,0 +1,64 @@
+# PYTHON PROGRAMMING
+# work with output
+# The idea of a program is to do something or carry out a task. 
+# In Python, we can use the print() function to display information on the screen.
+
+# Syntax:
+# print(expression)
+
+# Example 1: Printing simple text message
+# sum = 1 + 2
+# print(sum)
+
+#print("Show this in the console")
+
+# PYTHON VARIABLES
+# Variables are names given to data so that it can be stored and accessed later. They hold values which can change during runtime.
+# below example makes a calculation and stores it in variables
+
+''' 
+sum = 1+ 2
+# variable SUM store in variables the value of 3
+
+difference = sum - 1 
+# Variable DIFFERENCE store in variables the result of subtraction between SUM and 1
+
+product = sum * 2 
+# variable PRODUCT store in variables the result of multiplication between SUM and 2 
+
+print(difference)
+print(product)
+
+'''
+
+# PYTHON ::  Data types
+# A variable assumes a data type. 
+# In above program, SUM gets the int data type
+# Examples of data types in python
+
+# NUMERIC type --> number, with or without decimals
+#                          --> int, float, complex, 
+# STRING   type --> sequence of characters (text), enclosed by single quotes ' ', or double quotes " ".
+# BOOLEAN type --> True or False
+# LIST     type --> ordered and changeable collection of items. It's like an array in other programming languages.
+# DICTIONARY type --> DICTIONARY is collection of key-value pairs. It's like a list but more flexible.
+
+planets_in_solar_system = 8 # int data type
+print(planets_in_solar_system)
+print(type(planets_in_solar_system))
+
+distance_to_alpha_centauri = 4.367 # float, lightyears
+print(distance_to_alpha_centauri)
+print(type(distance_to_alpha_centauri))
+
+is_python_awesome = True # boolean data type
+print(is_python_awesome)
+print(type(is_python_awesome))
+
+shuttle_landed_on_the_moon = "Apollo 11" #string
+print(shuttle_landed_on_the_moon)
+print(type(shuttle_landed_on_the_moon))
+
+
+
+
