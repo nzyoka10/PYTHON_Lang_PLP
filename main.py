e82a1a4021b6ff243b17720c40cee2ec087985e8
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-28 16:54:21 +0300

    working with output in python

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 '''
     # Reading keyboard input
     This tutorial shows how to read user's keyboard input. The code is written in Python and displaying it on the screen.
@@ -14,5 +16,29 @@
 # print(name)
 
 # Converting numbers to strings
-x = 5
-print('The number is ' + str(x))
+# x = 5
+# print('The number is ' + str(x))
+'''
+Assignment operators
+You can use assignment operators to assign values to a variable throughout the lifecycle of the variable. Here are some assignment operators you're likely to encounter as you're learning to build programs:
+
+Operator	Example
+    =	x = 2
+    x now contains 2.
+    
+    +=	x += 2
+    x incremented by 2. If it contained 2 before, it now has a value of 4.
+    
+    -=	x -= 2
+    x decremented by 2. If it contained 2 before, it now has a value of 0.
+    
+    /=	x /= 2
+    x divided by 2. If it contained 2 before, it now has a value of 1.
+    
+    *=	x *= 2
+    x multiplied by 2. If it contained 2 before, it now has a value of 4.
+'''
+
+print("Today's date is: ", date.today())
+
+
