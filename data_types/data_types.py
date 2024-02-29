886f991fba76716b327f55a274f37ed4ead54aea
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-29 10:12:50 +0300

    more of string data type

---
+++ data_types/data_types.py
@@ -0,0 +1,53 @@
+# String data type
+# literal assignment
+
+first = "Kenya"
+last = "Kwanza"
+
+# print(type(pizza))
+# print(type(first) == str)
+# print(isinstance(first, bool))
+# print(isinstance(first, str))
+
+# # print(type(last) == int)
+# # print(isinstance(last, str))
+
+# constructor function
+# pizza = str("Pepperoni")
+# print(pizza)
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+# String Concatenation
+# concatenating strings using the + operator or the join() method.
+full_name =  first + " " + last
+print(full_name, "\n")
+
+# Casting a number to string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music from the " + decade + "s. \n"
+print(statement)
+
+# Using f-string formatting (Python version 3.6 and above)
+# formatted_statement = f"I like rock music from the {decade}s."
+# print(formatted_statement)
+
+# Multiple lines string
+multiline = ''' 
+Hey, how are you?
+
+I was just checking in.
+
+                All good!
+'''
+
+print(multiline)
+
+
+
+
+
