6ebba4ff4f056cbd945205d2731e67dc0f4e23cb
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-07 07:28:52 +0300

    updated files

---
+++ assignments/x02_week_2/task_2.py
@@ -0,0 +1,29 @@
+# Create a tuple containing the names of five of your favorite books. 
+# Then, use a for loop to print each book name on a separate line.
+
+#  for statement in Python
+# word = "Eric Doe"
+# for i in word:
+#     print(i)
+    
+# words = ['Cat', 'Mouth', 'Deference']
+# for w in words:
+#     print(w, len(w))
+
+# tuple of favorite books. 
+# print(isinstance(favorite_books, tuple))
+
+print("\n My Favorite Books: ")
+favorite_books = ('4 Hour Chef', 'Trace Wire', 'Power', 'Chicago PD', 'Nairobi Half Life')
+
+for book in favorite_books:
+    print(book, '\n')
+
+
+# print(type(favorite_books))
+# print(favorite_books)
+
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,11 @@
+# python loops
+#  python if statement
+# a=4
+# b=4
+
+# if a > b:
+#     print('Hello, a is greater than b')
+# elif b > a:
+#     print('Hello, a is less than b')
+# else:
+#     print("They are Equal")
