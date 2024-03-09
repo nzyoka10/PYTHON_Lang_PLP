66618b8e4c7a07cbd4fc88bb164541e1660c6868
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-09 14:30:50 +0300

    python loops

--- loops.py
+++ loops.py
@@ -1,11 +1,41 @@
-# python loops
-#  python if statement
-# a=4
-# b=4
+# 
+# ! ---_____  == ___  Python Loops ___ == ___----
+#  for --- loop 
+#  while --- loop
+# 
+# * For example:
+# names = ["Kenya", "United States", "China", "Tanzania", "Somalia"]
 
-# if a > b:
-#     print('Hello, a is greater than b')
-# elif b > a:
-#     print('Hello, a is less than b')
-# else:
-#     print("They are Equal")
+# for name in names:
+#     print('\n', name)
+
+# * For Loops : using Range
+#   - The range() function returns a sequence of numbers. 
+#   - It can take one, two or three arguments to define the range of values to generate. 
+
+#  example
+# welcome_msg = "Welcome to PLP"
+
+# for i in range(5):                    # Loop will run 5 times
+#     print('\n', welcome_msg)
+    
+# ** While Loops
+#   - A while loop is used when we don't know how many times the loop needs to be repeated. 
+''' 
+    #!  Syntax
+    # 
+    # *   while condition:
+    # *         code block to execute
+    # 
+'''
+# Example:
+# count = 0
+
+# while count < 10:                        # This loop will continue until count becomes equal to 10
+#     print("\n Count Value: ", count)       # Printing the value of count
+#     count += 1                          # Increasing the value of count by 1 after each iteration
+# print("\n\nLoop Ended")                # This message will be printed once the loop ends
+
+
+
+
