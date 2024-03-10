f2221e262ef62bf3804095eb582bca93b9bc001b
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-10 09:00:35 +0300

    python logical operations

---
+++ operators/logical.py
@@ -0,0 +1,21 @@
+# 
+# * Python Logical Operators
+# 
+
+print('\n ______   Python Logical Operators:  ________')
+print('\n p = True , p = False')
+
+p = True
+q = False
+
+print('\n Result of p AND q is: ', p and q)  # Output: False
+print('\n Result of q AND p is: ', q and p)  # Output: False
+print('\n')
+
+print('\n Result of p OR q is : ', p or q)   # Output: True
+print('\n Result of q OR p is : ', q or p)   # Output: True
+print('\n')
+
+print('\n Result of  NOT p is : ',  not p)    # Output: False
+print('\n Result of  NOT q is : ',  not q)    # Output: False
+
