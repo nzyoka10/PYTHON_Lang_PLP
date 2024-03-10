6d14faefe9020e5e721ef3cceb94637dec3daa85
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-10 09:16:41 +0300

    membership operators in python

---
+++ operators/membership.py
@@ -0,0 +1,11 @@
+# 
+# Python Membership operator
+# 
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print('\n Original List : ', list)
+
+print('\n Is 2 in my List : ', 2 in list)  # Output: True
+print('\n Is 6 not in my List : ', 6 not in list)  # Output: True
+print('\n Is 16 not in my List : ', 16 not in list)  # Output: True
+print('\n Is 0 not in my List : ', 0 not in list)  # Output: True
