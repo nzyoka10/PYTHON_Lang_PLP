f3b6b3a5036790386a5d624cd89d0cacb6abfc40
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-10 09:09:04 +0300

    python assignment operators

---
+++ operators/assignment.py
@@ -0,0 +1,19 @@
+# 
+# Assignment Operators
+# 
+
+x = 5
+x += 3  # Equivalent to x = x + 3
+print(x)  # Output: 8
+
+print('\n')
+
+y = "Hello"
+y += ", World!"  # String concatenation. Outputs Hello, World!
+print(y)
+
+print('\n')
+
+k = 7
+k /= 6
+print(k)
