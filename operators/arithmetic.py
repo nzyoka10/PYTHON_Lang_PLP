ab0fd52b0d4afcf4069fc098171248c7e807bb35
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-10 08:25:02 +0300

    python arithmetic operations

---
+++ operators/arithmetic.py
@@ -0,0 +1,14 @@
+# 
+# Examples of Arithmetic operations
+# 
+
+a = 10
+b = 3
+
+print('\n Sum: ', a + b)  
+print('\n Difference: ', a - b)  
+print('\n Product: ', a * b)  
+print('\n Division: ', a / b)  
+print('\n Reminder: ',  a % b)  
+print('\n Floor division:', a // b) 
+print('\n Exponentiation: ', a ** b) 
