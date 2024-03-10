706bade134c86e62ea56f0a6d1bc9ce761669ef0
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-10 08:38:49 +0300

    python operators

---
+++ operators/app.py
@@ -0,0 +1,15 @@
+
+# Python operators
+# ~ Arithmetic operators
+# 
+
+a = 10
+b = 3
+
+print(a + b)  # Output: 13
+print(a - b)  # Output: 7
+print(a * b)  # Output: 30
+print(a / b)  # Output: 3.3333333333333335
+print(a % b)  # Output: 1
+print(a // b) # Output: 3
+print(a ** b) # Output: 1000
