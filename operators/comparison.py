470f46509305ca9038a25c4dca4ad5001135c605
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-03-10 08:37:52 +0300

    comparison operators in python

---
+++ operators/comparison.py
@@ -0,0 +1,23 @@
+''' 
+== Equal to
+!= Not equal to
+< Less than
+> Greater than
+<= Less than or equal to
+>= Greater than or equal to
+'''
+
+a = 5
+b = 7
+
+print("\n a = 5 and b = 7 ")
+
+print('\n Is {a == b}: ',  a == b)  # Output: False
+print('\n Is {a != b} : ', a != b)  # Output: True
+print('\n Is {a < b} : ', a < b)   # Output: True
+print('\n Is {a > b} : ', a > b)   # Output: False
+print('\n Is {a <= b} : ', a <= b)  # Output: True
+print('\n Is {a >= b} : ', a >= b)  # Output: False
+
+
+
